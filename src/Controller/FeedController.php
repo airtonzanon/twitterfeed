f0906617968be66095997bbb02e2cdeb26b1394e
Airton <airtonzanon@gmail.com>
2016-09-29 15:24:23 -0300

    Adicionada Data/hora do Tweet e da última atualização do feed

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -11,12 +11,13 @@ class FeedController
 
     public function generateFeed($q, $count, $lang)
     {
+        date_default_timezone_set('America/Sao_Paulo');
         $search = SearchService::search($q, $count, $lang);
 
         $loader = new Twig_Loader_Filesystem(__DIR__ . '/../View');
         $twig = new Twig_Environment($loader);
 
-        return $twig->render('feed.xml', array('search' => $q, 'response' => $search));
+        return $twig->render('feed.xml', array('search' => $q, 'response' => $search, 'curdate' => date("Y-m-d\TH:i:s\Z")));
     }
 
 }
